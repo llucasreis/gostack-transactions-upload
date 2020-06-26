7cfd08e78b15d26ddae8394dd384d3b947595a05
lprs110 <pro.lucasreis@gmail.com>
2020-06-26 15:48:06 -0400

    feat: add create and delete routes

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { EntityRepository, Repository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
@@ -8,10 +9,51 @@ interface Balance {
   total: number;
 }
 
+interface TransactionsWithBalance {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  public async getBalance(): Promise<Balance> {
-    // TODO
+  public async findWithBalance(): Promise<TransactionsWithBalance> {
+    const transactions = await this.find({ relations: ['category'] });
+    const balance = await this.getBalance(transactions);
+
+    return {
+      transactions,
+      balance,
+    };
+  }
+
+  public async getBalance(transactions?: Transaction[]): Promise<Balance> {
+    if (!transactions) {
+      transactions = await this.find();
+    }
+    const { income, outcome } = transactions.reduce(
+      (acm: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            acm.income += transaction.value;
+            break;
+          case 'outcome':
+            acm.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return acm;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
@@ -8,14 +11,35 @@ import { Router } from 'express';
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+  const transactionsWithBalance = await transactionsRepository.findWithBalance();
+
+  return response.json(transactionsWithBalance);
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute(id);
+
+  return response.status(204).send();
   // TODO
 });
 
