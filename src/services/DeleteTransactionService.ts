baf90b7b2efb8fa2ef600bf9cc8cc272d686f9d6
lprs110 <pro.lucasreis@gmail.com>
2020-06-26 15:51:48 -0400

    fix: add error validation in delete route

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,13 +1,19 @@
-// import AppError from '../errors/AppError';
-
 import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
 import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
   public async execute(id: string): Promise<void> {
     const transactionsRepository = getCustomRepository(TransactionsRepository);
 
-    await transactionsRepository.delete(id);
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction not found', 404);
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
